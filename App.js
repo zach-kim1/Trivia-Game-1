d7728adcb4170b0273b5f2b7a4ae670cd102f9ac
zach-kim1 <zak7ab@virginia.edu>
2021-04-14 01:18:15 -0400

    component

--- App.js
+++ App.js
@@ -1,82 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
+import QuestionForm from "./components/QuestionForm";
 
 //import QuestionButtons from "./components/QuestionButtons";
 
 function App() {
-  let green = "#39D1B4";
-  let black = "#000000";
-  const [buttonColor, setButtonColor] = useState(black);
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10")
-      .then((res) => res.json())
-      .then((res) => {
-        //console.log(res);
-        setQuestions(res.results);
-
-      })
-
-  }, []);
-
-  function handleColorChange(e) {
-    e.preventDefault()
-    const newColor=green
-    //editGreen(questions.index, newColor)
-    setButtonColor(newColor);
-
-    
-  }
+      fetch("https://opentdb.com/api.php?amount=10")
+        .then((res) => res.json())
+        .then((res) => {
+          //console.log(res);
+          setQuestions(res.results);
   
-  /*
-  function editGreen(index,newColor){
-    const updatedQuestions = questions.map(question => {
-    if (index === question.index) {
-      setButtonColor(newColor);
-    }
-    })
-    setQuestions(updatedQuestions)
-  }
-  */
- 
-  return (
+        })
+  
+    }, []);
+const questionList = questions.map((question) => (
+  <QuestionForm
+    questionBody = {question.question}
+    correct = {question.correct_answer}
+    incorrect1 = {question.incorrect_answers[0]}
+    incorrect2 ={question.incorrect_answers[1]}
+    incorrect3 = {question.incorrect_answers[2]}
+  />
+)
+);
+return (
   <div>
-      <h1>Hey, Launch! 👋</h1> 
-      {questions.map((question) => (
-      <h2
-      >
-      {question.question}
-        <div>
-        <button_group> 
-            <button 
-            id = {"CA-" + nanoid()}
-            style={{ color : buttonColor}}
-            color ={buttonColor}
-            onClick = { handleColorChange }
-            >
-            {question.correct_answer}
-            </button>
-            <button
-            style={{
-              backgroundColor: 'red',
-            }}>{question.incorrect_answers[0]}
-            </button>        
-            <button
-            style={{
-              backgroundColor: 'red',
-            }}>
-              {question.incorrect_answers[1]}
-            </button>        
-            <button
-            style={{
-              backgroundColor: 'red',
-            }}>
-              {question.incorrect_answers[2]}
-              </button>        
-          </button_group>
-        </div>
-      </h2>
-      ))}      
+      {questionList}
+
   </div>
   );
 }
